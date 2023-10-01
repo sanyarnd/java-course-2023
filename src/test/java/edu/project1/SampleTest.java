95e8a351aa94ad37cc335202d9c3f40bd4a36c67
Vladislav <vladoss7700@mail.ru>
2023-10-01 15:41:22 +0300

    Remove template folder

---
+++ src/test/java/edu/project1/SampleTest.java
@@ -0,0 +1,23 @@
+package edu.project1;
+
+import edu.hw1.EvenArrayUtils;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class SampleTest {
+    @Test
+    @DisplayName("Фильтрация четных чисел")
+    void filterEvenNumbers() {
+        // given
+        int[] numbers = new int[] {1, 2, 3, 4, 5};
+
+        // when
+        int[] evenNumbers = EvenArrayUtils.filter(numbers);
+
+        // then
+        assertThat(evenNumbers)
+            .containsExactly(2, 4)
+            .hasSize(2);
+    }
+}
