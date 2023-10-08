b23625a07844a841cfb88233d912b1bfd5d7567d
Ruslan <noname0263@gmail.com>
2023-10-08 17:58:06 +0700

    add Task8

---
+++ project-template/src/main/java/edu/hw1/Task8_ChessKnights.java
@@ -0,0 +1,39 @@
+package edu.hw1;
+
+public class Task8_ChessKnights {
+
+    public static boolean isKnightBoardCapture(int[][] board){
+        //ALSO RETURN FALSE IF THE DATA IS INCORRECT
+        if(board.length != 8) return false;
+
+        for(int i = 0; i < 8; i++){
+            if(board[i].length != 8) return false;
+
+            for (int j = 0; j < 8; j++){
+                if(board[i][j] == 0) continue;
+                if(board[i][j] != 1) return false;
+
+                //VERTICALS FIRST
+                //up-left
+                if(i <= 5 && j != 0) if(board[i+2][j-1] == 1) return false;
+                //up-right
+                if(i <= 5 && j != 7) if(board[i+2][j+1] == 1) return false;
+                //down-left
+                if(i >= 2 && j != 0) if(board[i-2][j-1] == 1) return false;
+                //down-right
+                if(i >= 2 && j != 7) if(board[i-2][j+1] == 1) return false;
+
+                //HORIZONTAL FIRST
+                //left-up
+                if(i != 7 && j >= 2) if(board[i+1][j-2] == 1) return false;
+                //left-down
+                if(i != 0 && j >= 2) if(board[i-1][j-2] == 1) return false;
+                //right-up
+                if(i != 7 && j <= 5) if(board[i+1][j+2] == 1) return false;
+                //right-down
+                if(i != 0 && j <= 5) if(board[i-1][j+2] == 1) return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ project-template/src/test/java/edu/hw1/Task8ChessKnightsTest.java
@@ -0,0 +1,67 @@
+package edu.hw1;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class Task8ChessKnightsTest {
+    @Test
+    @DisplayName("1 Тест с курса")
+    void test1() {
+        // given
+        int[][] board = {
+            {0, 0, 0, 1, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 1, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 1, 0, 1, 0},
+            {0, 1, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 1, 0, 0, 0, 0, 0, 1},
+            {0, 0, 0, 0, 1, 0, 0, 0}
+        };
+        // when
+        boolean test = Task8_ChessKnights.isKnightBoardCapture(board);
+        // then
+        assertThat(test).isEqualTo(true);
+    }
+
+    @Test
+    @DisplayName("2 Тест с курса")
+    void test2() {
+        // given
+        int[][] board = {
+            {1, 0, 1, 0, 1, 0, 1, 0},
+            {0, 1, 0, 1, 0, 1, 0, 1},
+            {0, 0, 0, 0, 1, 0, 1, 0},
+            {0, 0, 1, 0, 0, 1, 0, 1},
+            {1, 0, 0, 0, 1, 0, 1, 0},
+            {0, 0, 0, 0, 0, 1, 0, 1},
+            {1, 0, 0, 0, 1, 0, 1, 0},
+            {0, 0, 0, 1, 0, 1, 0, 1}
+        };
+        // when
+        boolean test = Task8_ChessKnights.isKnightBoardCapture(board);
+        // then
+        assertThat(test).isEqualTo(false);
+    }
+
+    @Test
+    @DisplayName("2 Тест с курса")
+    void test3() {
+        // given
+        int[][] board = {
+            {0, 0, 0, 0, 1, 0, 0, 0},
+            {0, 0, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 1, 0, 0, 0, 0},
+            {1, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 1, 0, 0, 0},
+            {0, 0, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 0, 1, 0, 0},
+            {1, 0, 0, 0, 0, 0, 0, 0}
+        };
+        // when
+        boolean test = Task8_ChessKnights.isKnightBoardCapture(board);
+        // then
+        assertThat(test).isEqualTo(false);
+    }
+}
