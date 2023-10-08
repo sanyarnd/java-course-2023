ea8c9a236a337f0e5e2efd3a0b32c634af20a387
Ruslan <noname0263@gmail.com>
2023-10-08 22:39:45 +0700

    Task3 checkstyle update

--- project-template/src/main/java/edu/hw1/Task3InsertedArray.java
+++ project-template/src/main/java/edu/hw1/Task3InsertedArray.java
@@ -1,22 +1,27 @@
 package edu.hw1;
 
-public class Task3InsertedArray {
-    public static boolean isInsertable(int[] arr1, int[] arr2){
-        if(arr1.length == 0 || arr2.length == 0)
+final class Task3InsertedArray {
+    private Task3InsertedArray() {
+    }
+
+    public static boolean isInsertable(int[] arr1, int[] arr2) {
+        if (arr1.length == 0 || arr2.length == 0) {
             return false;
-        int min1, min2, max1, max2;
-
-        min1 = min2 = Integer.MAX_VALUE;
-        max1 = max2 = Integer.MIN_VALUE;
-
-        for (int var : arr1) {
-            min1 = Math.min(var, min1);
-            max1 = Math.max(var, max1);
         }
 
-        for (int var : arr2) {
-            min2 = Math.min(var, min2);
-            max2 = Math.max(var, max2);
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+
+        for (int item : arr1) {
+            min1 = Math.min(item, min1);
+            max1 = Math.max(item, max1);
+        }
+
+        for (int item : arr2) {
+            min2 = Math.min(item, min2);
+            max2 = Math.max(item, max2);
         }
 
         return min1 > min2 && max1 < max2;
