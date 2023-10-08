bc893d21b7b420900eb1b25427a7e2196f673fd0
Ruslan <noname0263@gmail.com>
2023-10-08 22:50:45 +0700

    Task5 checkstyle update

--- project-template/src/main/java/edu/hw1/Task5SpecialPalindrome.java
+++ project-template/src/main/java/edu/hw1/Task5SpecialPalindrome.java
@@ -1,21 +1,27 @@
 package edu.hw1;
 
-public class Task5SpecialPalindrome {
-    public static boolean isPalindrome(int number) {
-        int originalNumber = number;
-        int reversedNumber = 0;
-
-        while (number > 0) {
-            int lastDigit = number % 10;
-            reversedNumber = reversedNumber * 10 + lastDigit;
-            number /= 10;
-        }
-
-        return originalNumber == reversedNumber;
+final class Task5SpecialPalindrome {
+    private Task5SpecialPalindrome() {
     }
 
-    public static boolean isSpecialPalindrome(int value) {
-        if (value <= 10) {
+    final static public int TEN = 10;
+
+    public static boolean isPalindrome(int number) {
+        int notOriginalNumber = number;
+        int reversedNumber = 0;
+
+        while (notOriginalNumber > 0) {
+            int lastDigit = notOriginalNumber % TEN;
+            reversedNumber = reversedNumber * TEN + lastDigit;
+            notOriginalNumber /= TEN;
+        }
+
+        return number == reversedNumber;
+    }
+
+    public static boolean isSpecialPalindrome(int originalValue) {
+        int value = originalValue;
+        if (value <= TEN) {
             return false;
         }
         if (isPalindrome(value)) {
@@ -26,21 +32,22 @@ public class Task5SpecialPalindrome {
 
         int lengthValue = Task2CountDigits.getCountDigits(value);
         var split = new int[lengthValue];
-        for (int i = lengthValue - 1; -1 < i; i--){
-            split[i] = value % 10;
-            value /= 10;
+        for (int i = lengthValue - 1; -1 < i; i--) {
+            split[i] = value % TEN;
+            value /= TEN;
         }
 
-        int newValue=0;
-        newValue += split[0]+split[1];
+        int newValue = 0;
+        newValue += split[0] + split[1];
 
-        for (int i = 2; i < lengthValue - 1; i += 2){
-            int add = split[i] + split[i+1];
+        for (int i = 2; i < lengthValue - 1; i += 2) {
+            int add = split[i] + split[i + 1];
 
-            if (add == 0)
-                newValue *= 10;
-            else
-                newValue += add * (int) Math.pow(10, Task2CountDigits.getCountDigits(newValue));
+            if (add == 0) {
+                newValue *= TEN;
+            } else {
+                newValue += add * (int) Math.pow(TEN, Task2CountDigits.getCountDigits(newValue));
+            }
         }
         return isSpecialPalindrome(newValue);
     }
