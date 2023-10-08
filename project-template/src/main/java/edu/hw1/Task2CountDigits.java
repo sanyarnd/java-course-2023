66bd2645cbf01a38efefb59b823de625165d91ba
Ruslan <noname0263@gmail.com>
2023-10-08 22:32:52 +0700

    Task2 checkstyle update

--- project-template/src/main/java/edu/hw1/Task2CountDigits.java
+++ project-template/src/main/java/edu/hw1/Task2CountDigits.java
@@ -1,15 +1,23 @@
 package edu.hw1;
 
-public class Task2CountDigits {
-    public static int getCountDigits(int value){
-        if(value == 0) return 1;
+final class Task2CountDigits {
+    private Task2CountDigits() {
+    }
+
+    final static public int TEN = 10;
+
+    public static int getCountDigits(int value) {
+        if (value == 0) {
+            return 1;
+        }
 
         var result = 0;
+        var countingValue = value;
 
-        while(value != 0){
-            value /= 10;
+        while (countingValue != 0) {
+            countingValue /= TEN;
             result++;
         }
-        return  result;
+        return result;
     }
 }
