d7b12aee3a6ee51b26437993af671619229d9094
vladislav77777 <88504619+vladislav77777@users.noreply.github.com>
2023-10-12 17:48:40 +0300

    Update Task4.java

--- src/main/java/edu/hw2/Task4.java
+++ src/main/java/edu/hw2/Task4.java
@@ -1,12 +1,9 @@
 package edu.hw2;
 
-//import java.util.logging.Logger;
-
 public final class Task4 {
     private Task4() {
     }
 
-//    private static final Logger LOGGER = Logger.getLogger(Task4.class.getName());
     private static final int THREE = 3;
 
     public static CallingInfo callingInfo() {
@@ -20,16 +17,6 @@ public final class Task4 {
             return null;
         }
     }
-
-//    public static void main(String[] args) {
-//        CallingInfo callingInfo = callingInfo();
-//        if (callingInfo != null) {
-//            LOGGER.info("ClassName: " + callingInfo.className());
-//            LOGGER.info("MethodName: " + callingInfo.methodName());
-//        } else {
-//            LOGGER.info("Не удалось получить информацию о вызывающем методе\n");
-//        }
-//    }
 
     public record CallingInfo(String className, String methodName) {
     }
