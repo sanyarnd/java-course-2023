b7e77bf3a0c9811ac2e716306885a3b853e2fd71
DucksInDrugs <ducksindrugs@gmail.com>
2023-10-14 22:09:33 +0400

    Complete first homework

--- project-template/src/main/java/edu/hw1/Main.java
+++ project-template/src/main/java/edu/hw1/Main.java
@@ -1,7 +1,10 @@
 package edu.hw1;
 
+import java.util.Arrays;
+import java.util.Collections;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import static java.lang.Math.abs;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -12,16 +15,140 @@ public final class Main {
     }
 
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        LOGGER.info("Hello and welcome!");
+        logInfo();
+    }
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 0; i <= 2; i++) {
+    public static void logInfo() {
+        LOGGER.info("Привет, мир!");
+    }
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            LOGGER.info("i = {}", i);
+    @SuppressWarnings("MagicNumber")
+    public static int videoLength(String length) {
+        String[] time = length.split(":");
+        if (time.length == 2) {
+            try {
+                int secondsInMinute = 60;
+                if (Integer.parseInt(time[0]) >= 0
+                    && Integer.parseInt(time[1]) >= 0
+                    && Integer.parseInt(time[1]) < secondsInMinute) {
+                    return Integer.parseInt(time[0]) * secondsInMinute + Integer.parseInt(time[1]);
+                }
+            } catch (NumberFormatException e) {
+                return -1;
+            }
         }
+        return -1;
+    }
+
+    public static int digitCount(int number) {
+        return number == 0 ? 1 : (int) Math.floor(Math.log10(abs(number)) + 1);
+    }
+
+    public static boolean isNestable(Integer[] nested, Integer[] container) {
+        int nestedMin = Collections.min(Arrays.asList(nested));
+        int nestedMax = Collections.max(Arrays.asList(nested));
+        int containerMin = Collections.min(Arrays.asList(container));
+        int containerMax = Collections.max(Arrays.asList(container));
+        return nestedMin > containerMin && nestedMax < containerMax;
+    }
+
+    public static String stringFix(String brokenString) {
+        char[] chars = brokenString.toCharArray();
+        if (chars.length > 1) {
+            for (int i = 0; i < chars.length - 1; i += 2) {
+                char tmp = chars[i];
+                chars[i] = chars[i + 1];
+                chars[i + 1] = tmp;
+            }
+        }
+        return String.valueOf(chars);
+    }
+
+    @SuppressWarnings("MagicNumber")
+    public static boolean isPalindrome(int number) {
+        String numberCopy = String.valueOf(abs(number));
+        int i = 0;
+        int j = numberCopy.length() - 1;
+
+        while (i < j) {
+
+            if (numberCopy.charAt(i) != numberCopy.charAt(j)) {
+                if (numberCopy.length() % 2 == 0) {
+                    int numberDescendant = 0;
+                    for (int k = 0; k < numberCopy.length(); k += 2) {
+                        numberDescendant += Math.pow(10, k / 2)
+                            * (numberCopy.charAt(k) - '0' + numberCopy.charAt(k + 1) - '0');
+                    }
+                    if (number >= 10) {
+                        numberCopy = String.valueOf(numberDescendant);
+                        i = -1;
+                        j = numberCopy.length();
+                    } else {
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    @SuppressWarnings("MagicNumber")
+    public static int kaprekar(int number) {
+        if (number == 6174 || number <= 1000 || number >= 9999) {
+            return 0;
+        } else {
+            String stringNumber = String.valueOf(number);
+            int[] num = new int[4];
+
+            for (int i = 0; i < stringNumber.length(); i++) {
+                num[i] = stringNumber.charAt(i) - '0';
+            }
+            Arrays.sort(num);
+            String regex = "\\[|\\]|,|\\s";
+            int minNum = Integer.parseInt(Arrays.toString(num).replaceAll(regex, ""));
+            for (int i = 0; i < num.length / 2; i++) {
+                int temp = num[i];
+                num[i] = num[num.length - i - 1];
+                num[num.length - i - 1] = temp;
+            }
+            int maxNum = Integer.parseInt(Arrays.toString(num).replaceAll(regex, ""));
+            if (maxNum - minNum != 0) {
+                return 1 + kaprekar(maxNum - minNum);
+            } else {
+                return 0;
+            }
+        }
+    }
+
+    public static int rotateRight(int n, int shift) {
+        String binaryStr = Integer.toBinaryString(n);
+        String shiftedStr =
+            binaryStr.substring(binaryStr.length() - shift) + binaryStr.substring(0, binaryStr.length() - shift);
+        return Integer.parseInt(shiftedStr, 2);
+    }
+
+    public static int rotateLeft(int n, int shift) {
+        String binaryStr = Integer.toBinaryString(n);
+        String shiftedStr = binaryStr.substring(shift) + binaryStr.substring(0, shift);
+        return Integer.parseInt(shiftedStr, 2);
+    }
+
+    public static boolean isKnightTakes(int[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == 1
+                    && ((i + 1 < board.length && j + 2 < board[0].length && board[i + 1][j + 2] == 1)
+                    || (i + 2 < board.length && j + 1 < board[0].length && board[i + 2][j + 1] == 1)
+                    || (i + 1 < board.length && j - 2 >= 0 && board[i + 1][j - 2] == 1)
+                    || (i + 2 < board.length && j - 1 >= 0 && board[i + 2][j - 1] == 1))) {
+                        return false;
+                }
+            }
+        }
+        return true;
     }
 }
--- project-template/src/test/java/edu/hw1/SampleTest.java
+++ project-template/src/test/java/edu/hw1/SampleTest.java
@@ -1,22 +1,96 @@
 package edu.hw1;
 
-import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class SampleTest {
     @Test
-    @DisplayName("Фильтрация четных чисел")
-    void filterEvenNumbers() {
-        // given
-        int[] numbers = new int[] {1, 2, 3, 4, 5};
+    void getVideoLength() {
+        Assertions.assertEquals(-1, Main.videoLength("mm:ss"));
+        Assertions.assertEquals(-1, Main.videoLength("-100:-100"));
+        Assertions.assertEquals(-1, Main.videoLength("-100:00"));
+        Assertions.assertEquals(-1, Main.videoLength(":-10"));
+        Assertions.assertEquals(-1, Main.videoLength("12:-10"));
+        Assertions.assertEquals(-1, Main.videoLength("10:60"));
+        Assertions.assertEquals(5999940, Main.videoLength("99999:00"));
+        Assertions.assertEquals(600, Main.videoLength("10:00"));
+    }
 
-        // when
-        int[] evenNumbers = EvenArrayUtils.filter(numbers);
+    @Test
+    void getDigitCount() {
+        Assertions.assertEquals(2, Main.digitCount(-10));
+        Assertions.assertEquals(2, Main.digitCount(10));
+        Assertions.assertEquals(1, Main.digitCount(0));
+    }
 
-        // then
-        assertThat(evenNumbers)
-            .containsExactly(2, 4)
-            .hasSize(2);
+    @Test
+    void getIsNested(){
+        Assertions.assertTrue(Main.isNestable(new Integer[]{1, 2, 3, 4}, new Integer[]{0, 5}));
+        Assertions.assertFalse(Main.isNestable(new Integer[]{1, 2, 3, 4}, new Integer[]{0, 4}));
+        Assertions.assertFalse(Main.isNestable(new Integer[]{1, 2, 3, 4}, new Integer[]{1, 5}));
+        Assertions.assertFalse(Main.isNestable(new Integer[]{1, 2, 3, 4}, new Integer[]{1, 4}));
+        Assertions.assertTrue(Main.isNestable(new Integer[]{3, 1}, new Integer[]{4, 0}));//Надо затестить нормальным образом
+        Assertions.assertFalse(Main.isNestable(new Integer[]{3, 1}, new Integer[]{3, 1}));//Надо затестить нормальным образом
+    }
+
+    @Test
+    void getFixedString(){
+        Assertions.assertEquals("", Main.stringFix(""));
+        Assertions.assertEquals("1", Main.stringFix("1"));
+        Assertions.assertEquals("This is a mixed up string.", Main.stringFix("hTsii  s aimex dpus rtni.g"));
+    }
+
+    @Test
+    void getIsPalindrome(){
+        Assertions.assertTrue(Main.isPalindrome(11211230));
+        Assertions.assertTrue(Main.isPalindrome(11));
+        Assertions.assertTrue(Main.isPalindrome(1));
+        Assertions.assertTrue(Main.isPalindrome(101));
+        Assertions.assertFalse(Main.isPalindrome(112));
+        Assertions.assertTrue(Main.isPalindrome(-11));
+    }
+
+    @Test
+    void getKaprekar(){
+        Assertions.assertEquals(0, Main.kaprekar(1000));
+        Assertions.assertEquals(0, Main.kaprekar(1111));
+        Assertions.assertEquals(0, Main.kaprekar(10000));
+        Assertions.assertEquals(0, Main.kaprekar(6174));
+        Assertions.assertEquals(5, Main.kaprekar(6621));
+    }
+
+    @Test
+    void getRotateRight(){
+        Assertions.assertEquals(1, Main.rotateRight(1, 1));
+        Assertions.assertEquals(4, Main.rotateRight(8, 1));
+    }
+
+    @Test
+    void getRotateLeft(){
+        Assertions.assertEquals(1, Main.rotateLeft(16, 1));
+        Assertions.assertEquals(6, Main.rotateLeft(17, 2));
+    }
+
+    @Test
+    void getKnights(){
+        Assertions.assertTrue(Main.isKnightTakes(new int[][]{
+            {0, 0, 0, 1, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 1, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 1, 0, 1, 0},
+            {0, 1, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 1, 0, 0, 0, 0, 0, 1},
+            {0, 0, 0, 0, 1, 0, 0, 0}}));
+        Assertions.assertFalse(Main.isKnightTakes(new int[][]{
+            {1, 0, 1, 0, 1, 0, 1, 0},
+            {0, 1, 0, 1, 0, 1, 0, 1},
+            {0, 0, 0, 0, 1, 0, 1, 0},
+            {0, 0, 1, 0, 0, 1, 0, 1},
+            {1, 0, 0, 0, 1, 0, 1, 0},
+            {0, 0, 0, 0, 0, 1, 0, 1},
+            {1, 0, 0, 0, 1, 0, 1, 0},
+            {0, 0, 0, 1, 0, 1, 0, 1}}));
     }
 }
