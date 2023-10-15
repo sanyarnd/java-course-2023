74d9a8be8d295ca3616318f22553f957efb30b5a
Alina Semidotchenko <96502823+illalinalli@users.noreply.github.com>
2023-10-15 20:44:39 +0400

    Сделала ДЗ 1 (задания + тесты). Код хранится в папке ..main\java\edu\hw1 и ..test\java\edu\hw1

--- project-template/src/main/java/edu/hw1/Main.java
+++ project-template/src/main/java/edu/hw1/Main.java
@@ -1,27 +1,219 @@
 package edu.hw1;
 
 import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import java.util.Arrays;
+import java.util.logging.Logger;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public final class Main {
-    private final static Logger LOGGER = LogManager.getLogger();
+    private static java.util.logging.Logger LOGGER = Logger.getLogger("aboba");
 
-    private Main() {
+    public static int minutesToSeconds(String time){
+        var strs = time.split(":");
+        var minutes = Integer.parseInt(strs[0]);
+        var seconds = Integer.parseInt(strs[1]);
+        if (seconds < 60){
+            return (minutes * 60) + seconds;
+        }
+        else{
+            return -1;
+        }
     }
 
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        LOGGER.info("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 0; i <= 2; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            LOGGER.info("i = {}", i);
+    public static int countDigits(long num){
+        if (num == 0) return 1;
+        int count = 0;
+        while(num != 0){
+            num = num/10;
+            count++;
         }
+        return count;
+    }
+
+    public static Boolean isNestable(int[] a1, int[] a2){
+        if (a1.length == 0 || a2.length == 0) return false;
+        var min_a1 = Arrays.stream(a1).min().getAsInt();
+        var max_a1 = Arrays.stream(a1).max().getAsInt();
+
+        var min_a2 = Arrays.stream(a2).min().getAsInt();
+        var max_a2 = Arrays.stream(a2).max().getAsInt();
+
+        if (min_a1 > min_a2 && max_a1 < max_a2){
+            return true;
+        }
+        return false;
+    }
+    public static String fixString(String str) {
+        if (str == null) {
+            return null;
+        }
+        char[] chars = str.toCharArray();
+        for (int i = 0; i < chars.length - 1; i += 2) {
+            char temp = chars[i];
+            chars[i] = chars[i + 1];
+            chars[i + 1] = temp;
+        }
+        return new String(chars);
+    }
+
+    // Task 5 ----------------------------------------------------
+    public static boolean isPalindromeDescendant(int num) {
+        String str = Integer.toString(num);
+        while (str.length() > 1) {
+            if (isPalindrome(str)) {
+                return true;
+            }
+            str = getNextDescendant(str);
+        }
+        return false;
+    }
+
+    public static boolean isPalindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static String getNextDescendant(String str) {
+        StringBuilder sb = new StringBuilder();
+        int start = 0;
+
+        for (int i = start; i < str.length() - 1; ) {
+            int sum = Character.getNumericValue(str.charAt(i)) + Character.getNumericValue(str.charAt(i + 1));
+
+            sb.append(sum);
+            start += 2;
+            i = start;
+        }
+        return sb.toString();
+    }
+// -------------------------------
+
+    public static int countK(int n) {
+        if (n == 6174) {
+            return 0;
+        }
+        int[] digits = new int[4];
+        for (int i = 0; i < 4; i++) {
+            digits[i] = n % 10;
+            n /= 10;
+        }
+        Arrays.sort(digits);
+        int asc = digits[0] * 1000 + digits[1] * 100 + digits[2] * 10 + digits[3];
+        int desc = digits[3] * 1000 + digits[2] * 100 + digits[1] * 10 + digits[0];
+        int diff = desc - asc;
+        return 1 + countK(diff);
+    }
+
+    // Task 7
+    public static int rotateLeft(int n, int shift){
+        String binaryStr = Integer.toBinaryString(n);
+        String shifted = binaryStr.substring(shift) + binaryStr.substring(0, shift);
+        return Integer.parseInt(shifted, 2);
+    }
+
+    public static int rotateRight(int n, int shift){
+        String binaryStr = Integer.toBinaryString(n);
+        String shifted = binaryStr.substring(binaryStr.length() - shift) + binaryStr.substring(0, binaryStr.length()-shift);
+        return Integer.parseInt(shifted, 2);
+    }
+    // ---------------------------------------
+
+    public static boolean knightBoardCapture(int[][] board) {
+        int n = board.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 1) {
+                    for (int k = i + 1; k < n; k++) {
+                        for (int l = 0; l < n; l++) {
+                            if (board[k][l] == 1) {
+                                int dx = Math.abs(k - i);
+                                int dy = Math.abs(l - j);
+                                if ((dx == 1 && dy == 2) || (dx == 2 && dy == 1)) {
+                                    return false;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        // Task 0
+        LOGGER.info("Привет, мир!");
+
+        // Task 1 - Длина видео
+        var minutes = "10:60";
+        System.out.println(minutesToSeconds(minutes));
+
+        //Task 2 - Количество цифр
+        long number = 123456;
+        System.out.println(countDigits(number));
+
+        // Task 3 - Вложенный массив
+        int[] arr1 = new int[]{1, 2, 3, 4};
+        int[] arr2 = new int[]{2, 3};
+        System.out.println(isNestable(arr1, arr2));
+
+        // Task 4 - Сломанная строка
+        String str = "123456";
+        System.out.println(fixString(str));
+
+        // Task 5 - Особый палиндром
+        System.out.println(isPalindromeDescendant(11211230));
+        //System.out.println(isPalindromeDescendant(13001120));
+        //System.out.println(isPalindromeDescendant(23336014));
+        //System.out.println(isPalindromeDescendant(11));
+
+        // Task 6 -  Постоянная Капрекара
+        System.out.println(countK(3524));
+
+        // Task 7 - Циклический битовый сдвиг
+        System.out.println(rotateRight(8, 1));
+        System.out.println(rotateLeft(16, 1));
+        System.out.println(rotateLeft(17, 2));
+
+        // Task 8
+        int[][] array1 = {
+            {0, 0, 0, 1, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 1, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 1, 0, 1, 0},
+            {0, 1, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 1, 0, 0, 0, 0, 0, 1},
+            {0, 0, 0, 0, 1, 0, 0, 0}
+        };
+        int[][] array2 = {
+            {1, 0, 1, 0, 1, 0, 1, 0},
+            {0, 1, 0, 1, 0, 1, 0, 1},
+            {0, 0, 0, 0, 1, 0, 1, 0},
+            {0, 0, 1, 0, 0, 1, 0, 1},
+            {1, 0, 0, 0, 1, 0, 1, 0},
+            {0, 0, 0, 0, 0, 1, 0, 1},
+            {1, 0, 0, 0, 1, 0, 1, 0},
+            {0, 0, 0, 1, 0, 1, 0, 1}
+        };
+        int[][] array3 = {
+            {0, 0, 0, 0, 1, 0, 0, 0},
+            {0, 0, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 1, 0, 0, 0, 0},
+            {1, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 1, 0, 0, 0},
+            {0, 0, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 0, 1, 0, 0},
+            {1, 0, 0, 0, 0, 0, 0, 0}
+        };
+        System.out.println(knightBoardCapture(array3));
     }
 }
---
+++ project-template/src/test/java/edu/hw1/MainTest.java
@@ -0,0 +1,223 @@
+package edu.hw1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void minutesToSeconds() {
+        assertEquals(90, Main.minutesToSeconds("1:30"));
+        assertEquals(0, Main.minutesToSeconds("0:00"));
+        assertEquals(3599, Main.minutesToSeconds("59:59"));
+    }
+    @Test
+    void testInvalidInput() {
+        assertEquals(3600, Main.minutesToSeconds("60:00"));
+        assertEquals(-1, Main.minutesToSeconds("1:60"));
+        assertEquals(-1, Main.minutesToSeconds("61:60"));
+        assertEquals(-1, Main.minutesToSeconds("1:61"));
+    }
+
+    @Test
+    void countDigits() {
+        assertEquals(1, Main.countDigits(0));
+        assertEquals(1, Main.countDigits(5));
+        assertEquals(2, Main.countDigits(42));
+        assertEquals(3, Main.countDigits(123));
+        assertEquals(4, Main.countDigits(1000));
+        assertEquals(10, Main.countDigits(9876543210L));
+    }
+
+    @Test
+    void isNestable() {
+        int[] a1 = {1, 2, 3};
+        int[] a2 = {0, 1, 2, 3, 4};
+        assertTrue(Main.isNestable(a1, a2));
+    }
+    @Test
+    public void testIsNestable2() {
+        int[] a1 = {0, 1, 2, 3, 4};
+        int[] a2 = {1, 2, 3};
+        assertFalse(Main.isNestable(a1, a2));
+    }
+
+    @Test
+    public void testIsNestable3() {
+        int[] a1 = {1, 2, 3};
+        int[] a2 = {4, 5, 6};
+        assertFalse(Main.isNestable(a1, a2));
+    }
+
+    @Test
+    public void testIsNestable4() {
+        int[] a1 = {1, 2, 3};
+        int[] a2 = {1, 2, 3};
+        assertFalse(Main.isNestable(a1, a2));
+    }
+
+    @Test
+    public void testIsNestable5() {
+        int[] a1 = {1, 2, 3};
+        int[] a2 = {1, 2, 3, 4};
+        assertFalse(Main.isNestable(a1, a2));
+    }
+
+    @Test
+    public void testIsNestable6() {
+        int[] a1 = {1, 2, 3};
+        int[] a2 = {1, 2, 3, 4, 5};
+        assertFalse(Main.isNestable(a1, a2));
+    }
+
+    @Test
+    public void testIsNestable7() {
+        int[] a1 = {1, 2, 3, 4, 5};
+        int[] a2 = {3, 4, 5};
+        assertFalse(Main.isNestable(a1, a2));
+    }
+
+    @Test
+    public void testIsNestable8() {
+        int[] a1 = {};
+        int[] a2 = {1, 2, 3};
+        assertFalse(Main.isNestable(a1, a2));
+    }
+
+    @Test
+    public void testIsNestable9() {
+        int[] a1 = {1, 2, 3};
+        int[] a2 = {};
+        assertFalse(Main.isNestable(a1, a2));
+    }
+    @Test
+    void fixString() {
+        assertNull(Main.fixString(null));
+    }
+    @Test
+    public void testFixString2() {
+        assertEquals("", Main.fixString(""));
+    }
+
+    @Test
+    public void testFixString3() {
+        assertEquals("a", Main.fixString("a"));
+    }
+
+    @Test
+    public void testFixString4() {
+        assertEquals("bA", Main.fixString("Ab"));
+        assertEquals("bAdC", Main.fixString("AbCd"));
+        assertEquals("badcfehg", Main.fixString("abcdefgh"));
+    }
+
+    @Test
+    void GetNextDescendantTest() {
+        assertEquals("37", Main.getNextDescendant("1234"));
+        assertEquals("816", Main.getNextDescendant("3579"));
+    }
+
+    @Test
+    void isPalindrome() {
+        assertFalse(Main.isPalindrome("78999387"));
+        assertTrue(Main.isPalindrome("5665"));
+        assertFalse(Main.isPalindrome("13001120"));
+        assertTrue(Main.isPalindrome("77"));
+        assertFalse(Main.isPalindrome("9869"));
+    }
+
+    @Test
+    void testIsPalindromeDescendant() {
+        assertTrue(Main.isPalindromeDescendant(11211230));
+        assertTrue(Main.isPalindromeDescendant(23336014));
+        assertFalse(Main.isPalindromeDescendant(12345));
+    }
+
+    @Test
+    void countK() {
+        assertEquals(0, Main.countK(6174));
+        assertEquals(3, Main.countK(1234));
+        assertEquals(3, Main.countK(4321));
+        assertEquals(3, Main.countK(9876));
+    }
+
+    @Test
+    void rotateLeft() {
+        assertEquals(15, Main.rotateLeft(15, 3));
+        assertEquals(33, Main.rotateLeft(48, 1));
+        assertEquals(1, Main.rotateLeft(16, 1));
+        assertEquals(1, Main.rotateLeft(8, 1));
+    }
+
+    @Test
+    void rotateRight() {
+        assertEquals(4, Main.rotateRight(8, 1));
+        assertEquals(8, Main.rotateRight(16, 1));
+        assertEquals(14, Main.rotateRight(14, 4));
+        assertEquals(3, Main.rotateRight(3, 1));
+    }
+
+    @Test
+    void knightBoardCapture() {
+        int[][] board1 = {
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 1, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0}
+        };
+        assertTrue(Main.knightBoardCapture(board1));
+
+        int[][] board2 = {
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 1, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 1},
+            {0, 0, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 1},
+            {0, 0, 1, 0, 0, 0, 0, 0}
+        };
+        assertFalse(Main.knightBoardCapture(board2));
+
+        int[][] board3 = {
+            {0, 1, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 1, 0},
+            {0, 0, 0, 0, 1, 0, 0, 0},
+            {0, 0, 0, 1, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 1},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 1, 0, 0, 0},
+            {1, 0, 0, 0, 0, 0, 0, 0}
+        };
+        assertFalse(Main.knightBoardCapture(board3));
+
+        int[][] board4 = {
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 1, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 1},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0}
+        };
+        assertTrue(Main.knightBoardCapture(board4));
+
+        int[][] board5 = {
+            {1, 0, 1, 0, 1, 0, 1, 0},
+            {0, 1, 0, 1, 0, 1, 0, 1},
+            {0, 0, 0, 0, 1, 0, 1, 0},
+            {0, 0, 1, 0, 0, 1, 0, 1},
+            {1, 0, 0, 0, 1, 0, 1, 0},
+            {0, 0, 0, 0, 0, 1, 0, 1},
+            {1, 0, 0, 0, 1, 0, 1, 0},
+            {0, 0, 0, 1, 0, 1, 0, 1}
+        };
+        assertFalse(Main.knightBoardCapture(board5));
+    }
+}
