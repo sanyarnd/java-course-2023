32d199157e1c52c7b95153092141c4a814fe4bfa
rukhasa <mr.khamazaev@mail.ru>
2023-10-21 00:33:39 +0400

    HW_1

--- project-template/src/main/java/edu/hw1/Main.java
+++ project-template/src/main/java/edu/hw1/Main.java
@@ -1,27 +1,279 @@
 package edu.hw1;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public final class Main {
-    private final static Logger LOGGER = LogManager.getLogger();
+// Use this editor to write, compile and run your Java code online
+import java.util.Arrays;
+import java.util.logging.Logger;
 
-    private Main() {
+class HelloWorld {
+    public static int minutesToSeconds(String time){
+        int n = time.length();
+        Boolean err = false;
+
+        if (time.charAt(n-3)!= ':'){
+            err = true;// wrong input
+        }
+        int secs = 0;
+        try {
+            secs = Integer.parseInt(time.substring(n-2));
+
+        }
+        catch (NumberFormatException e) {
+            err = true;
+        }
+        if (secs>59 || secs<0){
+            err = true;
+        }
+        int mins = 0;
+        try {
+            mins = Integer.parseInt(time.substring(0, n-3));
+        }
+        catch (NumberFormatException e) {
+            err=true;// wrong input
+        }
+        if (err){
+            return -1;
+        }
+        return mins*60 + secs;
+
+        //return n;
+    }
+    public static int countDigits(int n){
+        String str = String.valueOf(n);
+        return str.length();
+    }
+    public static Boolean isNestable(int[] a, int[] b){
+        int n = a.length;
+        int m = b.length;
+        int minA = a[0];
+        int minB = b[0];
+        int maxA = a[0];
+        int maxB = b[0];
+
+        for (int i=0; i<n;++i){
+            if (a[i]<minA){
+                minA = a[i];
+            }
+            if(a[i]>maxA){
+                maxA=a[i];
+            }
+        }
+        for (int i=0; i<m;++i){
+            if (b[i]<minB){
+                minB = b[i];
+            }
+            if(b[i]>maxB){
+                maxB = b[i];
+            }
+        }
+        return maxA < maxB && minA>minB;
+
+    }
+    public static String fixString(String str){
+        int n = str.length();
+
+        char[] newstr = new char[n];
+        if (n%2 == 1){
+
+            n--;
+            newstr[n]=str.charAt(n);
+        }
+        for (int i = 0; i < n; i+=2){
+            newstr[i] = str.charAt(i+1);
+            newstr[i+1] = str.charAt(i);
+        }
+        return new String(newstr);
+    }
+    public static Boolean isPalindromeDescendant(int num){
+        String s = String.valueOf(num);
+        int n = s.length();
+        if (n<2){
+            return false;
+        }
+        Boolean isPal = true;
+        int newnum = 0;
+        int[] power = {1,10,100,1000,10000};
+        for(int i = 0; i < n; i+=2)
+        {
+            //System.out.println(i/2 - 1);
+            newnum+= (s.charAt(i) - '0' + s.charAt(i+1) - '0') * power[(n-i)/2 - 1];
+        }
+        for(int i = 0; i < n/2 ; i++)
+        {
+            if(s.charAt(i) != s.charAt(n-i-1)){
+                isPal = false;
+                break;
+            }
+        }
+        //System.out.println(newnum);
+        return isPal || isPalindromeDescendant(newnum);
+    }
+    public static void helloWorld(){
+        java.util.logging.Logger logger
+            = Logger.getLogger(
+            HelloWorld.class.getName());
+
+        // Call info method
+        logger.info("Привет, Мир!");
     }
 
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        LOGGER.info("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 0; i <= 2; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            LOGGER.info("i = {}", i);
+    public static int countK(int n){
+        int kapnum = 6174;
+        if (n == kapnum){
+            return 0;
         }
+        var a = new int[4];
+        int[] power = {1,10,100,1000};
+        for(int i = 0; i < 4; ++i){
+            a[i] = n/power[i]%10;
+        }
+        Arrays.sort(a);
+
+        int newnum = 0;
+        for(int i= 0; i < 4; ++i){
+            newnum+= (a[i]-a[3-i] )*power[i];
+        }
+        //System.out.println(newnum);
+        //return 0;
+        return countK(newnum) + 1;
+
+
+    }
+    public static int rotateLeft(int n, int shift){
+        int len=0;
+        int x = n;
+        while(x>0){
+            x/=2;
+            len++;
+        }
+        var power = new int[len+1];
+        power[0] = 1;
+        for(int i= 1; i < len+1; ++i){
+            power[i] = power[i-1]*2;
+        }
+        while(shift>0){
+            Boolean flag= false;
+            n*=2;
+            if(n>power[len]-1){
+                flag = true;
+            }
+
+            if(flag){
+                n-=power[len];
+                n+=1;
+                //System.out.println(n);
+            }
+
+            shift--;
+
+        }
+        return n;
+    }
+    public static int rotateRight(int n, int shift){
+        int len=0;
+        int x = n;
+        while(x>0){
+            x/=2;
+            len++;
+        }
+        var power = new int[len+1];
+        power[0] = 1;
+        for(int i= 1; i < len+1; ++i){
+            power[i] = power[i-1]*2;
+        }
+        while(shift>0){
+            Boolean flag= false;
+            if(n%2==1){
+                flag = true;
+            }
+            n/=2;
+            if(flag){
+                n+=power[len-1];
+                //System.out.println(n);
+            }
+            else{
+                len--;
+            }
+            shift--;
+
+        }
+        return n;
+    }
+    public static Boolean knightBoardCapture(int[][] field){
+        int n = 8;
+
+        for(int i = 0; i < n; ++i){
+            for(int j = 0; j < n; j++){
+                if(field[i][j]==1){
+                    for(int x = 0; x < n; ++x){
+                        for(int y = 0; y < n; y++){
+                            if(field[x][y]==1){
+                                if(Math.abs(x-i) ==2 && Math.abs(y-j)==1 ||Math.abs(x-i) ==1 && Math.abs(y-j)==2  ){
+                                    return false;
+                                }
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        System.out.println(minutesToSeconds("324:12"));
+        int[] a = { 1,2,3,4};
+        int[] b = {2,3};
+        int[][] chess = {
+            {0, 0, 0, 1, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 1, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 1, 0, 1, 0},
+            {0, 1, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 1, 0, 0, 0, 0, 0, 1},
+            {0, 0, 0, 0, 1, 0, 0, 0}};
+        int[][] chess2 = {
+            {1, 0, 1, 0, 1, 0, 1, 0},
+            {0, 1, 0, 1, 0, 1, 0, 1},
+            {0, 0, 0, 0, 1, 0, 1, 0},
+            {0, 0, 1, 0, 0, 1, 0, 1},
+            {1, 0, 0, 0, 1, 0, 1, 0},
+            {0, 0, 0, 0, 0, 1, 0, 1},
+            {1, 0, 0, 0, 1, 0, 1, 0},
+            {0, 0, 0, 1, 0, 1, 0, 1}
+        };
+        int[][] chess3 = {
+            {0, 0, 0, 0, 1, 0, 0, 0},
+            {0, 0, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 1, 0, 0, 0, 0},
+            {1, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 1, 0, 0, 0},
+            {0, 0, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 0, 1, 0, 0},
+            {1, 0, 0, 0, 0, 0, 0, 0}
+        };
+        System.out.println(isNestable(a,b));
+        System.out.println(fixString("badce"));
+        System.out.println(fixString("123456"));
+        System.out.println(fixString("hTsii  s aimex dpus rtni.g"));
+        System.out.println(countK(6621));
+        System.out.println(countK(6554));
+        System.out.println(countK(1234));
+        System.out.println(rotateLeft(16, 1));
+        System.out.println(rotateRight(15, 2));
+        System.out.println(rotateRight(15, 2));
+        System.out.println(knightBoardCapture(chess));
+        System.out.println(knightBoardCapture(chess2));
+        System.out.println(knightBoardCapture(chess3));
+        System.out.println(isPalindromeDescendant(11211230));//isPalindromeDescendant(13001120)
+        System.out.println(isPalindromeDescendant(13001120));
+        System.out.println(isPalindromeDescendant(23336014));
+        helloWorld();
+
+
+
+
+
     }
 }
--- project-template/src/test/java/edu/hw1/SampleTest.java
+++ project-template/src/test/java/edu/hw1/SampleTest.java
@@ -1,22 +1,110 @@
 package edu.hw1;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+class hw1Test {
 
-public class SampleTest {
-    @Test
-    @DisplayName("Фильтрация четных чисел")
-    void filterEvenNumbers() {
-        // given
-        int[] numbers = new int[] {1, 2, 3, 4, 5};
+    @org.junit.jupiter.api.Test
+    void Task1() {
+        assert (HelloWorld.minutesToSeconds("01:00") == 60);
+        assert (HelloWorld.minutesToSeconds("13:56") == 836);
+        assert (HelloWorld.minutesToSeconds("10:60") == -1);
+    }
 
-        // when
-        int[] evenNumbers = EvenArrayUtils.filter(numbers);
+    @org.junit.jupiter.api.Test
+    void Task2() {
+        assert (HelloWorld.countDigits(4666)== 4);
+        assert (HelloWorld.countDigits(544)== 3);
+        assert (HelloWorld.countDigits(0)== 1);
 
-        // then
-        assertThat(evenNumbers)
-            .containsExactly(2, 4)
-            .hasSize(2);
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void Task3() {
+        int[] a = { 1,2,3,4};
+        int[] b = {0,6};
+        int[] a2 = { 3, 1};
+        int[] b2 = {4, 0};
+        int[] a3= { 9, 9, 8};
+        int[] b3 = {8, 9};
+        int[] a4= { 1, 2, 3, 4};
+        int[] b4 = {2, 3};
+        /*isNestable([3, 1], [4, 0]) -> true
+isNestable([9, 9, 8], [8, 9]) -> false
+isNestable([1, 2, 3, 4], [2, 3]) -> false */
+        assert HelloWorld.isNestable(a,b);
+        assert HelloWorld.isNestable(a2,b2);
+        assert !HelloWorld.isNestable(a3,b3) ;
+        assert !HelloWorld.isNestable(a4,b4) ;
+    }
+
+    @org.junit.jupiter.api.Test
+    void Task4() {
+        assert HelloWorld.fixString("badce").equals("abcde");
+        assert HelloWorld.fixString("123456").equals("214365");
+        assert HelloWorld.fixString("hTsii  s aimex dpus rtni.g").equals("This is a mixed up string.");
+    }
+
+    @org.junit.jupiter.api.Test
+    void Task5() {
+        assert HelloWorld.isPalindromeDescendant(11211230);
+        assert HelloWorld.isPalindromeDescendant(13001120);
+        assert HelloWorld.isPalindromeDescendant(23336014);
+    }
+
+    @org.junit.jupiter.api.Test
+    void Task6() {
+        assert HelloWorld.countK(6621) == 5;
+        assert HelloWorld.countK(6554) == 4;
+        assert HelloWorld.countK(1234) == 3;
+    }
+
+    @org.junit.jupiter.api.Test
+    void Task7() {
+        assert HelloWorld.rotateLeft(16,1) == 1;
+        assert HelloWorld.rotateLeft(17,2) == 6;
+        assert HelloWorld.rotateRight(8,1) == 4;
+    }
+
+
+
+    @org.junit.jupiter.api.Test
+    void Task8() {
+        int[][] chess = {
+            {0, 0, 0, 1, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 1, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 1, 0, 1, 0},
+            {0, 1, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 1, 0, 0, 0, 0, 0, 1},
+            {0, 0, 0, 0, 1, 0, 0, 0}};
+        int[][] chess2 = {
+            {1, 0, 1, 0, 1, 0, 1, 0},
+            {0, 1, 0, 1, 0, 1, 0, 1},
+            {0, 0, 0, 0, 1, 0, 1, 0},
+            {0, 0, 1, 0, 0, 1, 0, 1},
+            {1, 0, 0, 0, 1, 0, 1, 0},
+            {0, 0, 0, 0, 0, 1, 0, 1},
+            {1, 0, 0, 0, 1, 0, 1, 0},
+            {0, 0, 0, 1, 0, 1, 0, 1}
+        };
+        int[][] chess3 = {
+            {0, 0, 0, 0, 1, 0, 0, 0},
+            {0, 0, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 1, 0, 0, 0, 0},
+            {1, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 1, 0, 0, 0},
+            {0, 0, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 0, 1, 0, 0},
+            {1, 0, 0, 0, 0, 0, 0, 0}
+        };
+        assert HelloWorld.knightBoardCapture(chess);
+        assert !HelloWorld.knightBoardCapture(chess2);
+        assert !HelloWorld.knightBoardCapture(chess3);
+    }
+
+    @org.junit.jupiter.api.Test
+    void main() {
     }
 }
