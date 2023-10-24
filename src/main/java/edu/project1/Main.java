52b13f86ed5ca354c36602da7cc5f6570c0b7fd7
Laviprog <areyouready@internet.ru>
2023-10-24 15:51:47 +0300

    update

--- src/main/java/edu/project1/Main.java
+++ src/main/java/edu/project1/Main.java
@@ -1,21 +1,9 @@
 package edu.project1;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 public final class Main {
     private Main() {
     }
 
     public static void main(String[] args) {
-        Output.welcome();
-        while (true){
-            Output.rules();
-            Output.menu();
-            Output.makeWord();
-            Play play = new Play(WordGen.wordGeneration());
-            if (!play.play()) break;
-        }
-        Output.bye();
     }
 }
